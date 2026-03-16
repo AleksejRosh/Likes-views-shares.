50ee72e24cbf870c309aea14f58bb13dfa655d7f
Aleksej <roshenkoaleksej@gmail.com>
2026-03-16 16:02:27 +0300

    Added the second dz2

---
+++ app/src/main/java/ru/netogy/myapplication/dto/Post.kt
@@ -0,0 +1,30 @@
+package ru.netogy.myapplication.dto
+
+data class Post (
+    val id: Int,
+    val author: String,
+    val published: String,
+    val content: String,
+    var likes: Int = 0,
+    var likedByMe: Boolean = false,
+    var shares: Int = 0
+)
+
+fun changeOne (a: Int): String {
+    return when {
+        a < 1_000 -> "$a"
+        a < 10_000 -> changeWithComma(a, 1_000, "K")
+        a < 100_000 -> changeWithoutComma(a)
+        else -> changeWithComma(a, 1_000_000, "M")
+    }
+}
+
+private fun changeWithComma(a: Int, c: Int, f: String): String {
+    val d = (a/c).toInt()
+    val e = ((a - d)/(c/10)).toInt()
+    return "$d" + ",$e" + f
+}
+private fun changeWithoutComma(a: Int): String {
+    val b = (a/1_000).toInt().toString()
+    return b + "K"
+}

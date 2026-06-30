91e7bb39a7607923c924049d0e24699727b6e9eb
Aleksej <roshenkoaleksej@gmail.com>
2026-06-30 21:04:52 +0300

    Added a scroll

--- app/src/main/java/ru/netogy/myapplication/fragment/PostFragment.kt
+++ app/src/main/java/ru/netogy/myapplication/fragment/PostFragment.kt
@@ -2,6 +2,7 @@ package ru.netogy.myapplication.fragment
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -15,6 +16,7 @@ import ru.netogy.myapplication.databinding.CardPostBinding
 import ru.netogy.myapplication.dto.Post
 import ru.netogy.myapplication.viewmodel.PostViewModel
 import kotlin.getValue
+
 
 class PostFragment: Fragment() {
     val viewModel: PostViewModel by activityViewModels()
